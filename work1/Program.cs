c9ccb8cc9acefce2cb2618bc340183e45498f924
Elena <lenalosyakova@gmail.com>
2022-10-17 22:22:03 +0300

    real numbers

---
+++ work1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m*n, заполненный вещественными числами
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Random random = new Random();
+            matr[i, j] = random.NextDouble()  + random.Next(-10, 10);
+        }
+    }
+}
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк (n): ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов (m): ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+double[,] matrix = new double [n, m];
+FillArray(matrix);
+PrintArray(matrix);
