99160050033f4825b6d57220d04447cfddfe3fc1
Elena <lenalosyakova@gmail.com>
2022-10-18 19:55:51 +0300

    m+n

---
+++ work2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать двумерный массив следующим правилом: A (mn) = m + n
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = i + j;
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк (n): ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов (m): ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int [n, m];
+FillArray(matrix);
+PrintArray(matrix);
