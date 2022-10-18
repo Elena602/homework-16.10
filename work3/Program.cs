1620221c6d512f24e3b2d38742d892940c044d1a
Elena <lenalosyakova@gmail.com>
2022-10-18 19:36:42 +0300

    replacing elements

---
+++ work3/Program.cs
@@ -0,0 +1,51 @@
+﻿// В двумерном массиве заменить элементы, у которых индексы четные, на нечетные
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Random random = new Random();
+            matr[i, j] = random.Next(-10, 10);
+        }
+    }
+}
+void ChangeArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                if (matr [i,j] % 2 == 0)
+                {
+                matr[i,j] = matr[i,j] + 1; 
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк (n): ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов (m): ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int [n, m];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+ChangeArray(matrix);
+PrintArray(matrix);
